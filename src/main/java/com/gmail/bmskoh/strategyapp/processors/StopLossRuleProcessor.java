45ad597400f8c35600972b8acee4d93ad76b209b
Leon Oh <redshampoo@gmail.com>
2020-02-12 12:40:57 +1100

    remove unused file

--- src/main/java/com/gmail/bmskoh/strategyapp/processors/StopLossRuleProcessor.java
+++ src/main/java/com/gmail/bmskoh/strategyapp/processors/StopLossRuleProcessor.java
@@ -2,6 +2,9 @@ package com.gmail.bmskoh.strategyapp.processors;
 
 import com.gmail.bmskoh.strategyapp.model.MarketTicker;
 
+/**
+ * This order processor decides when to trigger order based on stop loss rule and current ticker data.
+ */
 public class StopLossRuleProcessor implements ITriggeringRuleProcessor {
 
     @Override
